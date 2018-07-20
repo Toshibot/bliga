e1543963c98c54ce69e027f6f4692129fdbfe16c
Toshi Stewart <toshi.stewart@gmail.com>
2018-07-20 13:10:26 +0200

    Fixed result scores

--- app/js/main.js
+++ app/js/main.js
@@ -208,13 +208,13 @@ function fixtureItem(array) {
                 '<div class="c-fixture__team js-fixture-team-1">' +
                     '<img class="js-team-img" src="' + kitImg(array.Team1.TeamName,array.Team2.TeamName,"Home") + '" />' +
                     '<span class="js-team-text">' + teamAbrev(array.Team1.TeamName) + '</span>' +
-                    '<span class="c-fixture__score js-score-text">' + array.team_A.score + '</span>' +
+                    '<span class="c-fixture__score js-score-text">' + array.MatchResults[1].PointsTeam1 + '</span>' +
                 '</div>' +
                 '<div class="c-fixture__vs">vs</div>' +
                 '<div class="c-fixture__team js-fixture-team-2">' +
                     '<img class="js-team-img" src="' + kitImg(array.Team1.TeamName,array.Team2.TeamName,"Away") + '" />' +
                     '<span class="js-team-text">' + teamAbrev(array.Team2.TeamName) + '</span>' +
-                    '<span class="c-fixture__score js-score-text">' + array.team_B.score + '</span>' +
+                    '<span class="c-fixture__score js-score-text">' + array.MatchResults[1].PointsTeam2 + '</span>' +
                 '</div>' +
                 '<div class="c-fixture__venue js-fixture-venue">' + array.Location.LocationStadium + '</div>' +
             '</div>'
