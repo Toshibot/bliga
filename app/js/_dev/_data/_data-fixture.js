f7325625a12b8a85a57843f5a27abb5783dd6edc
Toshi Stewart <toshi.stewart@gmail.com>
2018-08-22 14:33:23 +0200

    split ajax queries out again

--- app/js/_dev/_data/_data-fixture.js
+++ app/js/_dev/_data/_data-fixture.js
@@ -2,12 +2,31 @@
 
 // Data - Fixture/Results
 
-function dataFixture(self) {
+function dataFixture() {
 
     // Variables
     var matchday = '';
     self.matchesURI = "https://api.football-data.org/v2/competitions/2002/matches";
     self.matchdayURI = "https://api.football-data.org/v2/competitions";
+    var self = this;
+
+    self.ajax = function(uri, method, data) {
+       var request = {
+          url: uri,
+          type: method,
+          accepts: "application/json",
+          cache: false,
+          dataType: "json",
+          data: JSON.stringify(data),
+          headers: {"X-Auth-Token": "5c8b70988e784fca8186b93d38b1bae7"},
+          error: function (jqXHR) {
+                console.log("ajax error " + jqXHR.status);
+          }
+ 
+       };
+ 
+       return $.ajax(request);
+    }
 
     // // Matchday
     // self.ajax(self.matchdayURI, 'GET').done(function(data){
--- app/js/_dev/_data/_data-ladder.js
+++ app/js/_dev/_data/_data-ladder.js
@@ -2,8 +2,28 @@
 //
 // Data
 // ====
-function dataLadder(self) { 
+function dataLadder() { 
 
+    var self = this;
+
+    self.ajax = function(uri, method, data) {
+       var request = {
+          url: uri,
+          type: method,
+          accepts: "application/json",
+          cache: false,
+          dataType: "json",
+          data: JSON.stringify(data),
+          headers: {"X-Auth-Token": "5c8b70988e784fca8186b93d38b1bae7"},
+          error: function (jqXHR) {
+                console.log("ajax error " + jqXHR.status);
+          }
+ 
+       };
+ 
+       return $.ajax(request);
+    }
+    
     self.tasksURI = "https://api.football-data.org/v2/competitions/2002/standings";
 
     self.ajax(self.tasksURI, 'GET').done(function(data) {
