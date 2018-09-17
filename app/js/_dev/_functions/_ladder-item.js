4cbb49a5fdac853cb68c08caf11dfcf8b971b17c
Toshi Stewart <toshi.stewart@gmail.com>
2018-09-17 12:21:17 +0200

    Fixed error with draws showing as losses in the ladder

--- app/js/_dev/_functions/_ladder-item.js
+++ app/js/_dev/_functions/_ladder-item.js
@@ -5,8 +5,8 @@ function ladderItem(array, number) {
     $('.c-ladder__item-' + number + ' div.c-ladder__team').children('img').attr('src', teamImg(array.team.name));  
     $('.c-ladder__item-' + number + ' div.c-ladder__played').text(array.playedGames);
     $('.c-ladder__item-' + number + ' div.c-ladder__wins').text(array.won);
-    $('.c-ladder__item-' + number + ' div.c-ladder__draws').text(array.draw);
     $('.c-ladder__item-' + number + ' div.c-ladder__losses').text(array.lost);
+    $('.c-ladder__item-' + number + ' div.c-ladder__draws').text(array.draw);
     $('.c-ladder__item-' + number + ' div.c-ladder__points-for').text(array.goalsFor);
     $('.c-ladder__item-' + number + ' div.c-ladder__points-against').text(array.goalsAgainst);
     $('.c-ladder__item-' + number + ' div.c-ladder__percentage').text(array.goalDifference);
